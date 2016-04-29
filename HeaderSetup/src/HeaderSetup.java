fb0c665236f98352f0acd11fc093fec4fba0ba0a
Søren <speder13@student.aau.dk>
2016-04-29 10:26:18 +0200

    fjams

--- HeaderSetup/src/HeaderSetup.java
+++ HeaderSetup/src/HeaderSetup.java
@@ -1,5 +1,20 @@
 /**
  * Created by Søren on 28-04-2016.
  */
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class HeaderSetup {
+//setup filewriter
+File outputFile = new File("output.java");
+    // creates the file
+    outputFile.createNewFile();
+    // creates a FileWriter Object
+    FileWriter writer = new FileWriter(outputFile);
+    // Writes the content to the file
+    writer.write("This\n is\n an\n example\n");
+    writer.flush();
+    writer.close();
 }
+
