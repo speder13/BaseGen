f696f966d6c430268081c992bb94127343fba19f
Jonas Stolberg <jStolberg>
2016-04-29 10:30:51 +0200

    Basic headergenerator

--- HeaderSetup/src/HeaderSetup.java
+++ HeaderSetup/src/HeaderSetup.java
@@ -1,20 +1,30 @@
-/**
- * Created by Søren on 28-04-2016.
- */
 
-import java.io.*;
+/*Offers functionality to build a Java file header*/
+
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class HeaderSetup {
-//setup filewriter
-File outputFile = new File("output.java");
-    // creates the file
-    outputFile.createNewFile();
-    // creates a FileWriter Object
-    FileWriter writer = new FileWriter(outputFile);
-    // Writes the content to the file
-    writer.write("This\n is\n an\n example\n");
-    writer.flush();
-    writer.close();
-}
 
+    //Builds, then returns, a header. One line per string in the list.
+    List<String> BuildHeader()
+    {
+        ArrayList<String> Header = new ArrayList<String>();
+        Header.add("import java.util.ArrayList;");
+        Header.add("import java.util.List;");
+        Header.add("");
+        Header.add("public class Main{");
+
+
+        System.out.println("The following has been written:");
+        for(Iterator<String> i = Header.iterator(); i.hasNext(); ) {
+            String item = i.next();
+            System.out.println("* " + item);
+        }
+
+        return Header;
+    }
+}
