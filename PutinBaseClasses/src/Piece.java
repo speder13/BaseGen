20743aa43f91919c1ed7b9308433662fc7f40b1a
Søren <speder13@student.aau.dk>
2016-04-29 11:58:20 +0200

    fjams

---
+++ PutinBaseClasses/src/Piece.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Søren on 29-04-2016.
+ */
+/*The SuperClass for Pieces*/
+
+public class Piece {
+}
---
+++ PutinBaseClasses/src/WinCondition.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Søren on 29-04-2016.
+ */
+/*The SuperClass for Winconditions*/
+public class WinCondition {
+}
